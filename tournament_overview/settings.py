24b7d85e52b7584a453307de1e53ea0e7cffc6a0
Aaren Rice <aaren.rice@gmail.com>
2026-08-01 12:18:55 -0500

    remove default superuser password

--- tournament_overview/settings.py
+++ tournament_overview/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("DJANGO_SECRET_KEY")
 
-SUPER_USER_PASSWORD = env("SUPER_USER_PASSWORD", default="password")
+SUPER_USER_PASSWORD = env("SUPER_USER_PASSWORD")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
